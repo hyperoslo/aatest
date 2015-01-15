068c2d5cbb26cc314c70c35b0fa7c45c79607115
Sindre Moen <sindrenm@gmail.com>
2015-01-15 15:17:58 +0100

    Add image preview option to Refile input

--- app/admin/image.rb
+++ app/admin/image.rb
@@ -3,9 +3,11 @@ ActiveAdmin.register Image do
   form do |f|
     f.inputs do
       f.input :title
+
       f.input :image, as: :refile, direct: true, host: request.base_url,
-        hint:(f.object.new_record? ? nil : f.template.image_tag(attachment_url(f.object, :image, :fill, 100, 100)))
-      end
+        image_preview: [ :fill, 175, 300 ], hint: 'Please upload awesome images'
+    end
+
     f.actions
   end
 
--- app/inputs/refile_input.rb
+++ app/inputs/refile_input.rb
@@ -1,6 +1,5 @@
 class RefileInput < Formtastic::Inputs::FileInput
   def file_input_html_options
-    options.delete :hint
     options[:data] ||= {}
 
     attacher = object.send(:"#{method}_attacher")
@@ -28,7 +27,31 @@ class RefileInput < Formtastic::Inputs::FileInput
 
   def to_html
     input_wrapping do
-      label_html << builder.hidden_field(method, hidden_input_html_options) << builder.file_field(method, file_input_html_options)
+      label_html <<
+      builder.hidden_field(method, hidden_input_html_options) <<
+      builder.file_field(method, file_input_html_options) <<
+      image_preview_content
     end
+  end
+
+  private
+
+  def image_preview_content
+    image_preview? ? image_preview_html(options[:image_preview]) : ''
+  end
+
+  def image_preview?
+    !!options[:image_preview]
+  end
+
+  def image_preview_html(options)
+    fill_method = options[0]
+    width       = options[1]
+    height      = options[2]
+
+    refile_app_path = Rails.application.routes.url_helpers.refile_app_path
+    url = Refile.attachment_url @object, method, fill_method, width, height, prefix: refile_app_path
+
+    template.image_tag url, class: 'image-preview'
   end
 end
